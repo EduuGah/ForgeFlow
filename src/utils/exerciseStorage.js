629ed0857dca49c1c2b713590e3435d45e654e46
Eduardo <edugah1809@gmail.com>
2026-05-04 18:34:44 -0300

    Agora exercicios antigos vão ser excluidos

--- src/utils/exerciseStorage.js
+++ src/utils/exerciseStorage.js
@@ -3,40 +3,33 @@ import defaultExercises from '../data/defaultExercises';
 const EXERCISES_KEY = 'forgeflow:exercises';
 const EXERCISES_VERSION_KEY = 'forgeflow:exercisesVersion';
 
-const CURRENT_EXERCISES_VERSION = '4';
+const CURRENT_EXERCISES_VERSION = '3';
 
 export function getInitialExercises() {
   const savedVersion = localStorage.getItem(EXERCISES_VERSION_KEY);
+
+  if (savedVersion !== CURRENT_EXERCISES_VERSION) {
+    localStorage.setItem(EXERCISES_KEY, JSON.stringify(defaultExercises));
+    localStorage.setItem(EXERCISES_VERSION_KEY, CURRENT_EXERCISES_VERSION);
+
+    return defaultExercises;
+  }
+
   const savedExercisesRaw = localStorage.getItem(EXERCISES_KEY);
 
   if (!savedExercisesRaw) {
     localStorage.setItem(EXERCISES_KEY, JSON.stringify(defaultExercises));
     localStorage.setItem(EXERCISES_VERSION_KEY, CURRENT_EXERCISES_VERSION);
+
     return defaultExercises;
   }
 
   try {
-    const savedExercises = JSON.parse(savedExercisesRaw);
-
-    if (savedVersion !== CURRENT_EXERCISES_VERSION) {
-      const savedIds = new Set(savedExercises.map((exercise) => exercise.id));
-
-      const newDefaultExercises = defaultExercises.filter(
-        (exercise) => !savedIds.has(exercise.id)
-      );
-
-      const mergedExercises = [...savedExercises, ...newDefaultExercises];
-
-      localStorage.setItem(EXERCISES_KEY, JSON.stringify(mergedExercises));
-      localStorage.setItem(EXERCISES_VERSION_KEY, CURRENT_EXERCISES_VERSION);
-
-      return mergedExercises;
-    }
-
-    return savedExercises;
+    return JSON.parse(savedExercisesRaw);
   } catch (error) {
     localStorage.setItem(EXERCISES_KEY, JSON.stringify(defaultExercises));
     localStorage.setItem(EXERCISES_VERSION_KEY, CURRENT_EXERCISES_VERSION);
+
     return defaultExercises;
   }
 }
