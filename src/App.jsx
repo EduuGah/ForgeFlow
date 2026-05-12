0df488baf721882158d3d29a517cebedb0951e86
Eduardo <edugah1809@gmail.com>
2026-05-12 15:54:56 -0300

    Retirado forgot-password²

--- src/App.jsx
+++ src/App.jsx
@@ -74,43 +74,6 @@ function App() {
                   </PublicRoute>
                 }
               />
-
-              <Route
-                path="/reset-password/:token"
-                element={
-                  <PublicRoute>
-                    <ResetPassword />
-                  </PublicRoute>
-                }
-              />
-
-              <Route path="/auth/callback" element={<AuthCallback />} />
-
-              <Route
-                element={
-                  <ProtectedRoute>
-                    <AppLayout />
-                  </ProtectedRoute>
-                }
-              >
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/exercises" element={<Exercises />} />
-                <Route path="/exercises/:id" element={<ExerciseDetails />} />
-                <Route path="/workouts" element={<Workouts />} />
-                <Route path="/history" element={<History />} />
-                <Route path="/start-workout" element={<StartWorkout />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/settings" element={<Settings />} />
-                <Route path="/complete-profile" element={<CompleteProfile />} />
-                <Route path="/calendar" element={<WorkoutCalendar />} />
-                <Route path="/recovery" element={<MuscleRecovery />} />
-                <Route path="/progress" element={<Progress />} />
-                <Route path="/exercise-progress" element={<ExerciseProgress />} />
-                <Route path="/progress-photos" element={<ProgressPhotos />} />
-                <Route path="/goals" element={<Goals />} />
-                <Route path="/notifications" element={<Notifications />} />
-                <Route path="/admin" element={<Admin />} />
-              </Route>
             </Routes>
           </Suspense>
         </WorkoutSessionProvider>
